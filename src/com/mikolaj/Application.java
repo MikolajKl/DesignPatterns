9a3e13f70b881129b1f87ea54c67b7dd25c22bcc
Mikolaj <mikolaj.kowal90@gmail.com>
2017-06-02 17:52:59 +0200

    Initial commit

---
+++ src/com/mikolaj/Application.java
@@ -0,0 +1,11 @@
+package com.mikolaj;
+
+/**
+ * Created by Notek on 2017-06-02.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
